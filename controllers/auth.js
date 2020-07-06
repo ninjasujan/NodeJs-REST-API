07cdb95c50b27f1c01d4f15aa9639ae76f37a0f9
sujan <sujanpoojary884@gmail.com>
2020-07-06 19:07:56 +0530

    signup done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.signUp = (req, res, next) => {
   const error = validationResult(req);
@@ -12,4 +13,26 @@ exports.signUp = (req, res, next) => {
   const name = req.body.name;
   const password = req.body.password;
   const email = req.body.email;
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      return user.save();
+    })
+    .then((user) => {
+      return res.status(201).json({
+        message: "User registered",
+        userId: user._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
